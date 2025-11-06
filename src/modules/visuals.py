57ea51c9edae72c55dab5cc4f0ea122c5acbfb5e
savimf <savi.phys@gmail.com>
2025-11-06 19:45:21 -0300

    Added folder structure and auxiliary files.

---
+++ src/modules/visuals.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+sns.set_theme(
+    style='white',
+    font='Serif',
+    rc={
+    'figure.figsize': [10, 5],
+    'figure.facecolor': 'white',
+    'figure.titlesize': 12,
+    'axes.axisbelow': False,
+	'axes.edgecolor': 'lightgrey',
+	'axes.facecolor': 'None',
+	'axes.grid': False,
+	'axes.labelcolor': 'black',
+    'axes.labelsize': 10,
+	'axes.spines.right': False,
+	'axes.spines.top': False,
+	'lines.solid_capstyle': 'round',
+	'patch.edgecolor': 'w',
+	'patch.force_edgecolor': True,
+	'text.color': 'black',
+	'xtick.bottom': False,
+	'xtick.color': 'black',
+	'xtick.direction': 'out',
+	'xtick.top': False,
+    'xtick.labelsize': 8,
+	'ytick.color': 'black',
+	'ytick.direction': 'out',
+	'ytick.left': False,
+	'ytick.right': False,
+    'ytick.labelsize': 8,
+    'legend.fontsize': 8,
+    'legend.title_fontsize': 10,
+    'legend.frameon': False
+    }
+)
+

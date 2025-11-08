9c83d0f405491b80167b8623cb252cda20df45ec
savimf <savi.phys@gmail.com>
2025-11-08 15:11:56 -0300

    Adding the first version of the pre processing notebook and updating utils.py, which now includes the functions: haversine_dist, get_min_distance and get_max_distance.

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from math import radians, cos, sin, asin, sqrt
 import params_cfg as pc
 
 brz_path = pc.BRZ_PATH
@@ -33,3 +35,63 @@ def process_lat_lon(df: pd.DataFrame, orig_names: list, prefix: str='') -> pd.Da
         df_[c] = df_[c].astype(float)
 
     return df_
+
+
+# feature engineering functions
+
+def haversine_dist(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
+    """Calculate the distance, in km, between two points, given their latitudes
+    and longitudes.
+    """
+    # convert decimal degrees to radians
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+
+    # apply haversine formula
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = sin(dlat/2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon/2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # Earth radius in km
+    return c * r
+
+
+def get_min_distance(lat: float, lon: float, stations: pd.DataFrame) -> float:
+    """Calculate the minimum distance from a given point (lat, lon) to a set of
+    police stations, with corresponding (lat, lon) coordinates.
+    """
+    df = stations.copy()
+
+    df['crime_lat'] = np.repeat(lat, len(df))
+    df['crime_lon'] = np.repeat(lon, len(df))
+
+    df['distance'] = df.apply(
+        lambda row: haversine_dist(
+            row["crime_lat"],
+            row["crime_lon"],
+            row["station_lat"],
+            row["station_lon"]
+        ),
+        axis=1
+    )
+    return df['distance'].min()
+
+
+def get_max_distance(lat: float, lon: float, stations: pd.DataFrame) -> float:
+    """Calculate the maximum distance from a given point (lat, lon) to a set of
+    police stations, with corresponding (lat, lon) coordinates.
+    """
+    df = stations.copy()
+
+    df['crime_lat'] = np.repeat(lat, len(df))
+    df['crime_lon'] = np.repeat(lon, len(df))
+
+    df['distance'] = df.apply(
+        lambda row: haversine_dist(
+            row["crime_lat"],
+            row["crime_lon"],
+            row["station_lat"],
+            row["station_lon"]
+        ),
+        axis=1
+    )
+    return df['distance'].max()
